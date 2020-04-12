01fddd2e63507eca3e2c36538e9ccc700d743241
Nicolas <nicolasfelix19@gmail.com>
2020-04-12 16:06:50 -0400

    Cambio de programacion movimiento en ruedas

---
+++ Sin Programar/Assets/Script/Auto.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Auto : MonoBehaviour
+{
+
+  
+    public float fzarotacion;
+    public bool adelante;
+    public bool atras;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputMovimiento();
+    }
+
+    void InputMovimiento()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+
+            transform.localRotation = new Quaternion(transform.rotation.x, transform.rotation.y + fzarotacion, transform.rotation.z, transform.rotation.w);
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+    public void MovimientoAuto(int index)
+    {
+        switch (index)
+        {
+            case 0:
+
+                
+
+
+                break;
+
+
+            case 1:
+               
+
+                break;
+
+        }
+
+
+
+    }
+}
---
+++ Sin Programar/Assets/Script/Wheel.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+
+    Rigidbody rgbd;
+    public float speed;
+
+   public bool adelante;
+    public bool atras;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RuedasDelanteras();
+
+
+        RuedasTraseras();
+    }
+
+
+
+    void RuedasTraseras()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+
+
+            rgbd.AddForce(Vector3.forward* speed, ForceMode.Impulse);
+
+
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+
+            rgbd.AddForce(Vector3.forward * -speed, ForceMode.Impulse);
+
+
+        }
+
+
+       
+
+
+    }
+
+
+
+    void RuedasDelanteras()
+    {
+
+       
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+
+            
+
+
+        }
+
+
+    }
+}
